0642f54c23bb597505dc2e82e2e37a01bec07dd1
pouya <pouya@applariat.com>
2017-06-19 16:21:06 -0700

    changed build.sh to check for war file

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM tomcat:latest
+
 ARG artifact_root="."
+
 COPY $artifact_root/build.sh /build.sh
 COPY $artifact_root/entrypoint.sh /entrypoint.sh
 COPY $artifact_root/src/ /src/
+
 RUN chmod +x /build.sh /entrypoint.sh && /build.sh
+
 ENTRYPOINT ["/entrypoint.sh"]
--- build.sh
+++ build.sh
@@ -3,7 +3,15 @@
 # Requirements - A valid war file exists within the code_dir
 # Copies the war file from the code_dir and copies it into /usr/local/tomcat/webapps
 
-#TO DO - Add an if to check for file and throw error if not present
+
 rm -rf /usr/local/tomcat/webapps/*
 
-cp -f $artifact_root/code/*.war /usr/local/tomcat/webapps
+#Check for a war file and throw error if not present
+if ls $artifact_root/src/*.war 1> /dev/null 2>&1
+then
+ echo "found a war file"
+ cp -f $artifact_root/src/*.war /usr/local/tomcat/webapps
+else
+ echo "ERROR! did not find any *.war file"
+ exit 1
+fi
