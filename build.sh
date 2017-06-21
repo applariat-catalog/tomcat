215a8174dd3b892dbbf3447cff2e0f204411000b
pouya <pouya@applariat.com>
2017-06-21 11:07:59 -0700

    using /code instead of /src, changing for user provided config file in code/tomcat-conf/

--- build.sh
+++ build.sh
@@ -9,24 +9,28 @@ logfile=/src/build.log
 exec > $logfile 2>&1
 set -x
 
-#make sure server.xml exists in /conf dir
-#stump the default server.xml file with the user provided config
-#optionally replace conf/server.xml with your own to customize tomcat
-if [ -e /conf/server.xml ]
+#check inside code artifact for /code/tomcat-conf/server.xml and use it if exists
+#customizing tomcat through providing tomcat-conf/server.xml as part of the code artifact
+if [ -e /code/tomcat-conf/server.xml ]
 then
- cp -f /conf/server.xml /usr/local/tomcat/conf/
+ cp -f /code/tomcat-conf/server.xml /usr/local/tomcat/conf/
 else
- echo "ERROR! did not find server.xml file in /conf folder"
- exit 1
+ #look inside /conf for server.xml and use it if exists
+ #customizing tomcat through cloning this github repo and providing conf/server.xml
+ if [ -e /conf/server.xml ]
+ then
+  cp -f /conf/server.xml /usr/local/tomcat/conf/
+ fi
+ #if non of the above, default config file inside the image will be used
 fi
 
 #Clean up potential leftovers
 rm -rf /usr/local/tomcat/webapps/*
 
 #Check for *.war files and throw error if not present
-if ls /src/*.war 1> /dev/null 2>&1
+if ls /code/*.war 1> /dev/null 2>&1
 then
- cp -f /src/*.war /usr/local/tomcat/webapps
+ cp -f /code/*.war /usr/local/tomcat/webapps
 else
  echo "ERROR! did not find any *.war file"
  exit 1
